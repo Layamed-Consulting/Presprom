9aaba2df1d96ca9cd076c30c1d5dcccaffd08051
Layamed-Consulting <mehdi.benjebara@outlook.com>
2026-03-05 13:20:00 +0000

    prom

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
     'depends': ['base','product','queue_job','sale_management', 'stock', 'account'],
     'data': [
         'views/promotion.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': True,
